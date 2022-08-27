0f31c29bd4edac22354871fff2037860c863f251
Quanquan Peng <bariona@qq.com>
2022-08-27 11:11:38 +0800

    Create, edit and delete item by form

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Movie-WatchList by Flask
 
-[Flask 教程](https://tutorial.helloflask.com/)
+[Flask 参考教程](https://tutorial.helloflask.com/)
+
+- `url_for`: 以视图函数名作为参数，返回对应的url
+
+  或者可以直接加载static文件夹中的静态文件
+
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 import click
 
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
@@ -16,6 +16,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'
+
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
@@ -69,18 +71,54 @@ def forge():
   
 @app.context_processor # 处理好每个模板都要传入的参数
 def inject_user():
-    user = User.query.first() # 读取用户数据
-    return dict(user=user)  # 根据context_processor的定义不难发现这里也要返回一个dict
-                            # 类似render_template('404.html', user=user)
+  user = User.query.first() # 读取用户数据
+  return dict(user=user)  # 根据context_processor的定义不难发现这里也要返回一个dict
+                          # 类似render_template('404.html', user=user)
                             
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+  if request.method == 'POST': # 获取表单数据 参考: https://tutorial.helloflask.com/form/#_3
+    title = request.form['title']
+    year = request.form['year']
+    if not title or not year or len(year) > 4 or len(title) > 60:
+      flash('Invalid Input.')
+      return redirect(url_for('index'))
+    movie = Movie(title=title, year=year)  # 创建记录
+    db.session.add(movie)  # 添加到数据库会话
+    db.session.commit()  
+    flash('Item created.')
+    return redirect(url_for('index'))  # 重定向回主页
+    
   movies = Movie.query.all()  # 读取所有电影记录
   return render_template('index.html', movies=movies)
   
-@app.route('/user/<name>')
-def user_page(name):
-  return f'User: {escape(name)}'
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+  movie = Movie.query.get_or_404(movie_id)
+
+  if request.method == 'POST':  # 处理编辑表单的提交请求
+    title = request.form['title']
+    year = request.form['year']
+
+    if not title or not year or len(year) != 4 or len(title) > 60:
+      flash('Invalid input.')
+      return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+    movie.title = title  # 更新标题
+    movie.year = year  # 更新年份
+    db.session.commit()  # 提交数据库会话
+    flash('Item updated.')
+    return redirect(url_for('index'))  # 重定向回主页
+
+  return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+  movie = Movie.query.get_or_404(movie_id)
+  db.session.delete(movie)
+  db.session.commit()
+  flash('Item Deleted.')
+  return redirect(url_for('index'))
 
 @app.errorhandler(404) # 接受404状态的异常
 def page_not_found(e):
